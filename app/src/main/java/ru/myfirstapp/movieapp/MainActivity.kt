900aff27531d3c2b2dd71766f132228f1633295e
Terentyev Alexander <ter.alex.vit@gmail.com>
2022-04-14 00:05:03 +0300

    Созданы фрагменты FragmentMoviesDetails.kt и FragmentMoviesList.kt, настроена навигация между ними.

--- app/src/main/java/ru/myfirstapp/movieapp/MainActivity.kt
+++ app/src/main/java/ru/myfirstapp/movieapp/MainActivity.kt
@@ -2,20 +2,32 @@ package ru.myfirstapp.movieapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import ru.myfirstapp.myapplication.MovieDetailsActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+//    private val fragmentMoviesList = FragmentMoviesList().apply { setListener(this@MainActivity)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.textview_start)
-        textView.setOnClickListener {
-            val intent = Intent(applicationContext, MovieDetailsActivity::class.java)
-            startActivity(intent)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.main_container, FragmentMoviesList())
+                .commit()
         }
     }
+
+//    override fun selectedAvengersEndGame() {
+//        supportFragmentManager.beginTransaction()
+//            .add(R.id.main_container, FragmentMoviesDetails())
+//            .addToBackStack(null)
+//            .commit()
+//    }
 }
