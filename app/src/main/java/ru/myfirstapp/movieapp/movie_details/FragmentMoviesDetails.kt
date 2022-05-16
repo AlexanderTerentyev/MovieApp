af500355e12b6e26180ad840c894c92fd28895b2
Terentyev Alexander <ter.alex.vit@gmail.com>
2022-05-16 19:25:01 +0300

    HomeWork #6. Архитектурный подход MVVM.

--- app/src/main/java/ru/myfirstapp/movieapp/movie_details/FragmentMoviesDetails.kt
+++ app/src/main/java/ru/myfirstapp/movieapp/movie_details/FragmentMoviesDetails.kt
@@ -8,18 +8,12 @@ import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import ru.myfirstapp.movieapp.R
-import ru.myfirstapp.movieapp.data.Movie
-import ru.myfirstapp.movieapp.data.loadMovies
 
 class FragmentMoviesDetails : Fragment() {
-
-    private val scope = CoroutineScope(Dispatchers.Default)
 
     companion object {
         const val MOVIE_ID = "MOVIE_ID"
@@ -35,6 +29,13 @@ class FragmentMoviesDetails : Fragment() {
 
     private val movieId: Int by lazy { requireNotNull(arguments?.getInt(MOVIE_ID)) }
 
+    private val movieDetailsViewModel: MovieDetailsViewModel by viewModels {
+        MyViewModelFactory(
+            requireActivity().application,
+            movieId
+        )
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -42,25 +43,21 @@ class FragmentMoviesDetails : Fragment() {
     ): View? = inflater.inflate(R.layout.fragment_movies_details, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        scope.launch(Dispatchers.Main) {
-            val recyclerActors = view.findViewById<RecyclerView>(R.id.recycler_actors)
-            val titleView = view.findViewById<TextView>(R.id.text_title)
-            val backdrop = view.findViewById<ImageView>(R.id.image_view_header)
-            val overview = view.findViewById<TextView>(R.id.text_view_description)
-            val minimumAge = view.findViewById<TextView>(R.id.text_view_age)
-            val ratings = view.findViewById<RatingBar>(R.id.rating_bar_movie_list)
-            val numberOfRatings = view.findViewById<TextView>(R.id.text_view_reviews)
-            val genres = view.findViewById<TextView>(R.id.text_view_genre)
-            val cast = view.findViewById<TextView>(R.id.text_cast)
-            val adapter = ActorsAdapter()
-            recyclerActors.adapter = adapter
-            recyclerActors.setHasFixedSize(true)
+        val recyclerActors = view.findViewById<RecyclerView>(R.id.recycler_actors)
+        val titleView = view.findViewById<TextView>(R.id.text_title)
+        val backdrop = view.findViewById<ImageView>(R.id.image_view_header)
+        val overview = view.findViewById<TextView>(R.id.text_view_description)
+        val minimumAge = view.findViewById<TextView>(R.id.text_view_age)
+        val ratings = view.findViewById<RatingBar>(R.id.rating_bar_movie_list)
+        val numberOfRatings = view.findViewById<TextView>(R.id.text_view_reviews)
+        val genres = view.findViewById<TextView>(R.id.text_view_genre)
+        val cast = view.findViewById<TextView>(R.id.text_cast)
 
-            val movies: List<Movie> = loadMovies(requireContext())
-            val movie: Movie = movies.first {
-                it.id == movieId
-            }
+        val adapter = ActorsAdapter()
+        recyclerActors.adapter = adapter
+        recyclerActors.setHasFixedSize(true)
 
+        movieDetailsViewModel.movie.observe(this.viewLifecycleOwner) { movie ->
             adapter.bindActors(movie.actors)
             titleView.text = movie.title
             Glide.with(view.context)
@@ -76,6 +73,9 @@ class FragmentMoviesDetails : Fragment() {
             } else {
                 cast.visibility = View.VISIBLE
             }
+
         }
+        movieDetailsViewModel.openMovieDetails()
+
     }
 }
